936c2f1fab6cf07566d138266c64546a67539f9d
Ankit Majee <ankitmajee.05@gmail.com>
2024-06-19 21:02:45 +0530

    Created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
 export default function App() {
   return (
-    <div>Upload here</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="Dashboard" element={<Dashboard />} />
+        <Route path="projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
