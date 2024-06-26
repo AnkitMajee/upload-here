16b9be0d58383dc10bf3da849a4827bc01961472
Ankit Majee <ankitmajee.05@gmail.com>
2024-06-26 21:51:58 +0530

    DB connected

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>{
+        console.log('Connected to MongoDB');
+    })
+    .catch((err)=>{
+        console.log('Error connecting to MongoDB', err);
+    });
 const app= express();
 
 app.get('/', (req, res) => {  
