2abdafc0de375d987b01a567371c9106e0f7be2d
Ankit Majee <ankitmajee.05@gmail.com>
2024-06-28 20:37:41 +0530

    adding middleware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,13 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorhandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {  //async is used to make the function asynchronous so that we can use await inside it
+export const signup = async (req, res, next) => {  //async is used to make the function asynchronous so that we can use await inside it
     //console.log(req.body); we will store it into a databse instead of console.log
 
     const {username, email, password} = req.body;
     if(!username || !email || !password || username === ''|| email===''||password===''){
-        return res.status(400).json({message: 'All fields are required'});
+        next(errorhandler(400,'All fields are required'));
     }
 
     const hashedPassword = await bcryptjs.hash(password, 10); //10 is the salt round
@@ -20,6 +21,6 @@ export const signup = async (req, res) => {  //async is used to make the functio
         await newuser.save(); //awiat is used to wait for the promise to be resolved
         res.json('Signup successful');
     } catch(error){
-        res.status(500).json({message: error.message})
+        next(error);
     }
 };
--- api/index.js
+++ api/index.js
@@ -15,10 +15,23 @@ mongoose.connect(process.env.MONGODB_URL)
 
 
 const app= express();
+
 app.use(express.json());
-app.use('/api/user',userRoutes); 
-app.use('/api/auth',authRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+app.use('/api/user',userRoutes); 
+app.use('/api/auth',authRoutes);
+
+app.use((err,req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
+
