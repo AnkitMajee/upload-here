85bfa147731a9d397ef6cdf6944e5f231bce62dd
Ankit Majee <ankitmajee.05@gmail.com>
2024-07-02 23:21:58 +0530

    Added redux in Signin frontend

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,14 @@
 import  { useState } from 'react'
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart, signInSuccess, signInFailure } from '../../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData]= useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading, error: errorMessage} = useSelector((state) => state.user);
   const navigate = useNavigate();  //after signup user redirected to sign-in page
-
+  const dispatch = useDispatch();
 
   const handleChange =(e) =>{
     setFormData({...formData, [e.target.id]: e.target.value.trim()});
@@ -15,11 +16,10 @@ export default function SignIn() {
   const handleSubmit = async (e) =>{ 
     e.preventDefault();
     if( !formData.email || !formData.password){
-      return setErrorMessage('Please fill in all fields');
+      return dispatch(signInFailure('Please fill all fields'));
     }
     try{
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('api/auth/signin', {
         method: 'POST',
         headers: {
@@ -29,15 +29,15 @@ export default function SignIn() {
       });
       const data = await res.json();
       if(data.success === false){
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
       setLoading(false);
       if(res.ok){        //after signup user redirected to sign-in page
+        dispatch(signInSuccess(data));
         navigate('/');
       }
     }catch(error){
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   };
   return <div className='min-h-screen mt-20'>
