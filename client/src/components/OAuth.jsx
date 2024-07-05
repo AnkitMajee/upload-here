0e88b74778fcbcd834cdbf055af7efb7eff0af30
Ankit Majee <ankitmajee.05@gmail.com>
2024-07-05 23:27:00 +0530

    Header Updated

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -3,7 +3,7 @@ import { AiFillGoogleCircle } from 'react-icons/ai'
 import { GoogleAuthProvider, signInWithPopup,getAuth } from 'firebase/auth'
 import {app} from '../firebase';
 import {useDispatch} from 'react-redux';
-import {signInSuccess} from '../redux/authSlice';
+import {signInSuccess} from '../redux/user/userSlice';
 import {useNavigate} from 'react-router-dom';
 
 export default function OAuth() {
