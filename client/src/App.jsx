c5f805f1bc0b9a5ca21fd3a74b152705ddc34f0f
Ankit Majee <ankitmajee.05@gmail.com>
2024-07-24 14:17:16 +0530

    Added CreatePost route and UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Projects from './pages/Projects'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import PrivateRoute from './components/PrivateRoute'
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 
 export default function App() {
   return (
@@ -21,6 +22,10 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="private" element={<PrivateRoute />}>
           <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:postId' element={<UpdatePost />} />
         </Route>
         <Route path="projects" element={<Projects />} />
       </Routes>
