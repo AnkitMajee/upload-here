60ff397d6d57acfa6b60ee101ec90cea28a276a4
Ankit Majee <ankitmajee.05@gmail.com>
2024-09-06 12:40:44 +0530

    Added UploadPost Feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import PrivateRoute from './components/PrivateRoute'
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
+import CreatePost from './pages/CreatePost';
+import UpdatePost from './pages/UpdatePost';
 
 export default function App() {
   return (
