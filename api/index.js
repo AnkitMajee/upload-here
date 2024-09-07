fef889c82e541c7454c76e5731913567d64d042e
Ankit Majee <ankitmajee.05@gmail.com>
2024-09-07 22:23:55 +0530

    added Comment on post feature

--- api/index.js
+++ api/index.js
@@ -4,40 +4,49 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URL)
-    .then(()=>{
-        console.log('Connected to MongoDB');
-    })
-    .catch((err)=>{
-        console.log('Error connecting to MongoDB', err);
-    });
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+const __dirname = path.resolve();
 
-const app= express();
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000!');
 });
 
-app.use('/api/user',userRoutes); 
-app.use('/api/auth',authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.use((err,req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
 // import PostPage from './pages/PostPage';
-// import ScrollToTop from './components/ScrollToTop';
+import ScrollToTop from './components/ScrollToTop';
 // import Search from './pages/Search';
 
 export default function App() {
